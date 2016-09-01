b0890a704402ebb431cfe9689938c79721d8fac2
Nik Vangerow <n@notro.uk>
2016-09-01 13:11:22 +0100

    Initial Commit

---
+++ PygmentsKit/PygmentsKit.h
@@ -0,0 +1,19 @@
+//
+//  PygmentsKit.h
+//  PygmentsKit
+//
+//  Created by Nik on 01/09/2016.
+//  Copyright © 2016 Niklas Vangerow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PygmentsKit.
+FOUNDATION_EXPORT double PygmentsKitVersionNumber;
+
+//! Project version string for PygmentsKit.
+FOUNDATION_EXPORT const unsigned char PygmentsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PygmentsKit/PublicHeader.h>
+
+
